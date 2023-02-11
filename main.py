a31c17c70c80fb856a9536635f7bed31ee351851
KesterDaniel <kesterdan17@gmail.com>
2023-02-11 15:49:17 +0100

    displaying cards

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from tkinter import *
 import pandas as pd
@@ -11,23 +12,31 @@ window.config(bg=BACKGROUND_COLOR, padx=30, pady=30)
 data = pd.read_csv("./data/french_words.csv")
 dictionary_list = [{row["French"]: row["English"]} for (index, row) in data.iterrows()]
 french_words = data["French"].to_list()
+print(type(french_words))
 
+game_on = True
 
+# Logic
+while game:
+    french_word = random.choice(french_words)
+    card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+    front_card = PhotoImage(file="./images/card_front.png")
+    card_canvas.create_image(400, 263, image=front_card)
+    lang_text = card_canvas.create_text(400, 180, text="French", fill="black", font=("Arial", 30, "bold"))
+    word_text = card_canvas.create_text(400, 300, text=french_word, fill="black", font=("Arial", 20, "bold"))
+    card_canvas.grid(row=0, column=0, columnspan=2)
+    game = False
 
 
 # UI setup
-card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-front_card = PhotoImage(file="./images/card_front.png")
-card_canvas.create_image(400, 263, image=front_card)
-card_canvas.grid(row=0, column=0, columnspan=2)
+# right_btn = PhotoImage(file="./images/right.png")
+# image_button = Button(window, image=right_btn, borderwidth=0)
+# image_button.grid(row=1, column=0)
 
-right_btn = PhotoImage(file="./images/right.png")
-image_button = Button(window, image=right_btn, borderwidth=0)
-image_button.grid(row=1, column=0)
+# wrong_btn = PhotoImage(file="./images/wrong.png")
+# image_button = Button(window, image=wrong_btn, borderwidth=0)
+# image_button.grid(row=1, column=1)
 
-wrong_btn = PhotoImage(file="./images/wrong.png")
-image_button = Button(window, image=wrong_btn, borderwidth=0)
-image_button.grid(row=1, column=1)
 
 
 window.mainloop()
