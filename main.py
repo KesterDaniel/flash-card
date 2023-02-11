6aa22a7abf9b42e9251795461b9d56add15ff025
KesterDaniel <kesterdan17@gmail.com>
2023-02-11 14:10:26 +0100

    arranged the data

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import time
 from tkinter import *
+import pandas as pd
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=30, pady=30)
 
+# Data Arrangement
+data = pd.read_csv("./data/french_words.csv")
+dictionary_list = [{row["French"]: row["English"]} for (index, row) in data.iterrows()]
+french_words = data["French"].to_list()
+
+
+
+
+# UI setup
 card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card = PhotoImage(file="./images/card_front.png")
 card_canvas.create_image(400, 263, image=front_card)
